7818192d99d3a26557c879019bec77a5c47a8357
sivaprasad15018 <45034246+sivaprasad15018@users.noreply.github.com>
2026-09-11 16:44:42 +0530

    Update hello method to return version 2 message

--- src/main/java/com/example/cicddemo/HelloController.java
+++ src/main/java/com/example/cicddemo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World - Version 1";
+        return "Hello World - Version 2";
     }
 }
